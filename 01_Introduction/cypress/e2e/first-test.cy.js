3ab7c1fa5da69d861205ce72086d023a3b8a06f7
tintoll <belselios@gmail.com>
2023-07-08 05:54:21 +0900

    간단한 테스트 작성

--- 01_Introduction/cypress/e2e/first-test.cy.js
+++ 01_Introduction/cypress/e2e/first-test.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+    // get은 특정 DOM 요소를 검색한다. 인자로 css selector를 넣어주면된다.
+    // should는 기대치를 검증, 첫번째인자는 기대치를 검증할 항목, 두번째인자는 값
+    cy.get("li").should("have.length", 6);
+  });
+});
