178f7b32f48b09f5edef9c36ea75c3ee7fdbda4f
tintoll <belselios@gmail.com>
2023-07-10 06:41:02 +0900

    title 찾아서 검증

--- 02_cypress_basics/cypress/e2e/basics.cy.js
+++ 02_cypress_basics/cypress/e2e/basics.cy.js
@@ -5,4 +5,11 @@ describe("tasks page", () => {
     cy.visit("http://localhost:5173/");
     cy.get(".main-header img");
   });
+
+  it("should display the page title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("h1").contains("My Cypress Course Tasks");
+
+    // cy.contains("My Cypress Course Tasks"); // 사이트 전체에서 글자가 포하모디어있는지 찾는다.
+  });
 });
