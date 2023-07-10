abe0b6fc9e4387b9c56a4cb997bc3b7bfb8d95e4
tintoll <belselios@gmail.com>
2023-07-10 23:16:19 +0900

    should를 이용한 명시적 검증 추가

--- 02_cypress_basics/cypress/e2e/basics.cy.js
+++ 02_cypress_basics/cypress/e2e/basics.cy.js
@@ -8,7 +8,10 @@ describe("tasks page", () => {
 
   it("should display the page title", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("h1").contains("My Cypress Course Tasks");
+
+    // h1을 다 찾는다. should를 이용해서 명시적으로 기대한 값을 검증한다.
+    cy.get("h1").should("have.length", 1);
+    cy.get("h1").contains("My Cypress Course Tasks"); // 묵시적 검증이 된다.
 
     // cy.contains("My Cypress Course Tasks"); // 사이트 전체에서 글자가 포하모디어있는지 찾는다.
   });
