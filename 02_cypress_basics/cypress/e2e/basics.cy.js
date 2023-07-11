d3a3cee7afc259d8a9e19713b4d85650c2bad387
tintoll <belselios@gmail.com>
2023-07-11 22:21:07 +0900

    find()메서드 이용하여 대상찾기

--- 02_cypress_basics/cypress/e2e/basics.cy.js
+++ 02_cypress_basics/cypress/e2e/basics.cy.js
@@ -3,7 +3,8 @@
 describe("tasks page", () => {
   it("should render the main image", () => {
     cy.visit("http://localhost:5173/");
-    cy.get(".main-header img");
+    // cy.get(".main-header img"); // 중첩해서 사용하는 것은 좋지 않다.
+    cy.get(".main-header").find("img"); // 이렇게 사용하면 정확히 원하는 것을 가져오지 않는다.
   });
 
   it("should display the page title", () => {
