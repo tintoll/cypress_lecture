f31b4789bb442724e8fb9ba8665f32955e049d59
tintoll <belselios@gmail.com>
2023-07-12 22:19:44 +0900

    배경 클릭시 모달 및 배경이 사라지는지 체크

--- 02_cypress_basics/cypress/e2e/tasks.cy.js
+++ 02_cypress_basics/cypress/e2e/tasks.cy.js
@@ -8,5 +8,9 @@ describe("tasks management", () => {
 
     // Modal 이 뜨고 모달 외 배경화면을 클릭하면 Modal이 사라진다.
     cy.get(".backdrop").click({ force: true }); // 그냥 클릭하면 동작하지 않는다. force를 true 해줘야한다.
+
+    // 실제 사라졌는지 확인
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
   });
 });
