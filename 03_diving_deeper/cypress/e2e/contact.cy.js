b182e69f35250ab8526c21223b8e76bdad43c062
tintoll <belselios@gmail.com>
2023-07-18 05:42:13 +0900

    스페셜키를 활용하여 저장

--- 03_diving_deeper/cypress/e2e/contact.cy.js
+++ 03_diving_deeper/cypress/e2e/contact.cy.js
@@ -5,13 +5,20 @@ describe("contact form", () => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello");
     cy.get('[data-cy="contact-input-name"]').type("joi");
-    cy.get('[data-cy="contact-input-email"]').type("test@example.com");
+    // cy.get('[data-cy="contact-input-email"]').type("test@example.com");
 
     cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
     cy.get('[data-cy="contact-btn-submit"]').should(
       "not.have.attr",
       "disabled"
     );
+
+    // then 메서드를 이용하여 같은 요소에 대한 검증을 묶어서 할 수 있다.
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      // 이곳에서 el은 말그대로 dom element이다. 그래서 검증시 expect를 이용해야한다.
+      expect(el.attr("disabled")).to.be.undefined;
+      expect(el.text()).to.eq("Send Message");
+    });
 
     // 함수를 체인닝해서 사용할 수 있다.
     cy.get('[data-cy="contact-btn-submit"]')
@@ -24,9 +31,11 @@ describe("contact form", () => {
 
     // const btn = cy.get('[data-cy="contact-btn-submit"]'); 변수에 get을 할 수는 있지만 실제 btn 객체가 담겨져 있지 않다.
 
+    cy.get('[data-cy="contact-input-email"]').type("test@example.com{enter}");
+
     // as 메서드를 이용하여 alias를 주어 사용하는 것을 권장한다.
     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
-    cy.get("@submitBtn").click();
+    // cy.get("@submitBtn").click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
   });
