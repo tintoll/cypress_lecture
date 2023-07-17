bc61ffd3287a1e9d17490adeee212eb6cb30fec4
tintoll <belselios@gmail.com>
2023-07-17 22:18:52 +0900

    contact form 입력 테스트

---
+++ 03_diving_deeper/cypress/e2e/contact.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe("contact form", () => {
+  it("should submit the form ", () => {
+    cy.visit("http://localhost:5173/about");
+    cy.get('[data-cy="contact-input-message"]').type("Hello");
+    cy.get('[data-cy="contact-input-name"]').type("joi");
+    cy.get('[data-cy="contact-input-email"]').type("test@example.com");
+
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+    cy.get('[data-cy="contact-btn-submit"]').should(
+      "not.have.attr",
+      "disabled"
+    );
+    cy.get('[data-cy="contact-btn-submit"]').click();
+
+    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
+    cy.get('[data-cy="contact-btn-submit"]').should("have.attr", "disabled");
+  });
+});
