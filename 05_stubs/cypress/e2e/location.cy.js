2a7a391cc4af468eb88e93a00463013b0ef273ab
tintoll <belselios@gmail.com>
2023-07-22 07:08:54 +0900

    fixture 기능을이용하여 공유데이터 생성

--- 05_stubs/cypress/e2e/location.cy.js
+++ 05_stubs/cypress/e2e/location.cy.js
@@ -2,6 +2,7 @@
 
 describe("share location", () => {
   beforeEach(() => {
+    cy.fixture("user-location.json").as("userLocation");
     cy.visit("/").then((win) => {
       // then에서 작업 하는 이유는 window 객체에 접근하지 못할 수 있어서 직업 받을 수 있는 then에서 작업한다.
       // stub는 첫번째 인자롤 사용할 개체, 두번째 인자로 대체할 메서드명을 입력한다.
@@ -10,20 +11,18 @@ describe("share location", () => {
       //   "getUserPosition"
       // );
 
-      // getCurrentPosition 메서드를 가짜로 구현하고 싶으면 resolve, return, callFake를 이용한다. 여기서는 callback이 필요하다
-      cy.stub(win.navigator.geolocation, "getCurrentPosition")
-        .as("getUserPosition")
-        .callsFake((cb) => {
-          // 원래 함수가 딜레이가 있어야 하느로 setTimeout으로 딜레이를 준다.
-          setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 37.5,
-                longitude: 48.01,
-              },
-            });
-          }, 100);
-        });
+      cy.get("@userLocation").then((fakePosition) => {
+        // getCurrentPosition 메서드를 가짜로 구현하고 싶으면 resolve, return, callFake를 이용한다. 여기서는 callback이 필요하다
+        cy.stub(win.navigator.geolocation, "getCurrentPosition")
+          .as("getUserPosition")
+          .callsFake((cb) => {
+            // 원래 함수가 딜레이가 있어야 하느로 setTimeout으로 딜레이를 준다.
+            setTimeout(() => {
+              cb(fakePosition);
+            }, 100);
+          });
+      });
+
       // wirteText가 promise를 반환하기 때문에 resolves를 사용한다.
       cy.stub(win.navigator.clipboard, "writeText")
         .as("saveToClipboard")
@@ -45,10 +44,13 @@ describe("share location", () => {
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@saveToClipboard").should("have.been.called");
 
-    // 올바른 값으로 전달되었는지 테스트
-    cy.get("@saveToClipboard").should(
-      "have.been.calledWithMatch",
-      new RegExp(`${37.5}.*${48.01}.*${encodeURI("John Doe")}`)
-    );
+    cy.get("@userLocation").then((fakePosition) => {
+      const { latitue, longitude } = fakePosition.coords;
+      // 올바른 값으로 전달되었는지 테스트
+      cy.get("@saveToClipboard").should(
+        "have.been.calledWithMatch",
+        new RegExp(`${latitue}.*${longitude}.*${encodeURI("John Doe")}`)
+      );
+    });
   });
 });
