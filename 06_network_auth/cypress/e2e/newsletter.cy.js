9b2f3fe870343b25cfff3d7a022bd7a5990de108
tintoll <belselios@gmail.com>
2023-08-01 10:57:01 +0900

    네트워크 처리 관련예제

---
+++ 06_network_auth/cypress/e2e/newsletter.cy.js
@@ -0,0 +1,35 @@
+describe("NewsLetter", () => {
+  beforeEach(() => {
+    cy.task("seedDatabase");
+  });
+
+  it("should display a success message", () => {
+    cy.intercept("POST", "/newsletter*", { status: 201 }).as("subscribe");
+    cy.visit("/");
+    cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait("@subscribe");
+    cy.contains("Thanks for signing up");
+  });
+
+  it("should display validation error", () => {
+    cy.intercept("POST", "/newsletter*", {
+      message: "Email exists already.",
+    }).as("subscribe");
+    cy.visit("/");
+    cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait("@subscribe");
+    cy.contains("Email exists already.");
+  });
+  it("should successfully create a new contact", () => {
+    cy.request({
+      method: "POST",
+      url: "/newsletter",
+      body: { email: "test@example.com" },
+      form: true,
+    }).then((res) => {
+      expect(res.status).to.eq(201);
+    });
+  });
+});
